2ee74c764eb2a553ac94ae0ae13c62673ed802c4
vu1405 <nhv.hungvu@gmail.com>
2026-06-02 11:59:17 +0000

    Update api.py

--- tv4_app/api.py
+++ tv4_app/api.py
@@ -1,22 +1,51 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
+
+from pyspark.sql import SparkSession
+from pyspark.ml import PipelineModel
 
 app = FastAPI()
 
-model = joblib.load("model.pkl")
+# Khởi tạo Spark
+spark = SparkSession.builder \
+    .appName("FakeNewsAPI") \
+    .master("local[*]") \
+    .getOrCreate()
+
+# Load model của TV3
+model = PipelineModel.load(
+    "../tv3_ml/saved_model/logistic_regression"
+)
 
 class InputText(BaseModel):
     text: str
 
 @app.post("/predict")
 def predict(data: InputText):
-    proba = model.predict_proba([data.text])[0]
-    classes = model.classes_
-    idx = proba.argmax()
-    label = classes[idx]
-    confidence = float(proba[idx])
-    return {"label": label, "confidence": confidence}
+
+    df = spark.createDataFrame(
+        [(data.text,)],
+        ["content"]
+    )
+
+    result = model.transform(df)
+
+    row = result.select(
+        "prediction",
+        "probability"
+    ).collect()[0]
+
+    prediction = int(row["prediction"])
+
+    probs = row["probability"]
+
+    confidence = float(probs[prediction])
+
+    return {
+        "prediction": prediction,
+        "label": "FAKE" if prediction == 1 else "REAL",
+        "confidence": confidence
+    }
 
 @app.get("/health")
 def health():
